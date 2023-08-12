7846c3de80665b55ac82af0140c05f88e380f50c
Winter_Snake <blizzardsnake98@gmail.com>
2023-08-12 17:11:10 -0400

    refactor: separate projects

--- JJx.Editor/Program.cs
+++ JJx.Editor/Program.cs
@@ -6,21 +6,20 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-
-namespace JJx;
+using JJx;
 
 internal class Program
 {
 	/* Static Methods */
 	private static async Task Main(string[] args)
 	{
-		Player player;
-		using (var fs = File.Open("debug/player/Player.dat", FileMode.Open, FileAccess.Read))
+		if (args.Length != 1) return;
+		JJx.Player player;
+		using (var fs = File.Open(args[0], FileMode.Open, FileAccess.Read))
 		{
 			player = await Player.FromStream(fs);
 		}
-		player.Gameplay.Flags = (Gameplay.Flag.Hardcore | Gameplay.Flag.ContinuousTime);
-		using (var fs = File.Open("debug/player/Debug.0.dat", FileMode.Open, FileAccess.Write))
+		using (var fs = File.Open(args[0], FileMode.Open, FileAccess.Write))
 		{
 			await player.ToStream(fs);
 		}
