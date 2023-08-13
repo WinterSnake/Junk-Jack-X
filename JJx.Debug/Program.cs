9195b04c21b69a1e126e8aa94e9e1a211b89036a
Winter_Snake <blizzardsnake98@gmail.com>
2023-08-13 15:02:07 -0400

    add: load/save on demand

--- JJx.Debug/Program.cs
+++ JJx.Debug/Program.cs
@@ -13,17 +13,44 @@ internal class Program
 	/* Static Methods */
 	private static async Task Main(string[] args)
 	{
-		if (args.Length != 1) return;
-		JJx.World world;
-		using (var fs = File.Open(args[0], FileMode.Open, FileAccess.Read))
+		if (args.Length < 2 || args.Length > 3) return;
+		// Player Editing
+		if (args[0] == "-p" || args[0] == "--player")
 		{
-			world = await World.FromStream(fs);
+			JJx.Player player;
+			// Read player from file
+			Console.WriteLine("Loading player from file...");
+			using (var fs = File.Open(args[1], FileMode.Open, FileAccess.Read))
+			{
+				player = await Player.FromStream(fs);
+			}
+			// Do changes
+			// Write player to file (if flagged)
+			if (args.Length == 2 || (args.Length == 3 && args[2] != "--store")) return;
+			Console.WriteLine("Writing player to file...");
+			using (var fs = File.Open(args[1], FileMode.Open, FileAccess.Write))
+			{
+				await player.ToStream(fs);
+			}
 		}
-		Console.WriteLine(world.Gamemode);
-		world.Gamemode = Gamemode.Survival;
-		using (var fs = File.Open(args[0], FileMode.Open, FileAccess.Write))
+		// World Editing
+		else if (args[0] == "-w" || args[0] == "--world")
 		{
-			world.ToStream(fs);
+			JJx.World world;
+			// Read world from file
+			Console.WriteLine("Loading world from file..");
+			using (var fs = File.Open(args[1], FileMode.Open, FileAccess.Read))
+			{
+				world = await World.FromStream(fs);
+			}
+			// Do changes
+			// Write world to file (if flagged)
+			if (args.Length == 2 || (args.Length == 3 && args[2] != "--store")) return;
+			Console.WriteLine("Writing world to file..");
+			using (var fs = File.Open(args[1], FileMode.Open, FileAccess.Write))
+			{
+				await world.ToStream(fs);
+			}
 		}
 	}
 }
