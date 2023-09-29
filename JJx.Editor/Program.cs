deb7260e43a8b194f7decdc53f9fc8fa83f2b834
Winter_Snake <blizzardsnake98@gmail.com>
2023-09-29 16:23:18 -0400

    add: screen state

--- JJx.Editor/Program.cs
+++ JJx.Editor/Program.cs
@@ -10,14 +10,19 @@ using System.Threading.Tasks;
 using Raylib_cs;
 using JJx;
 
+internal enum Screen : byte
+{
+	Main = 0,
+	Player = 1,
+	World = 2
+}
+
 internal static class Program
 {
 	/* Static Methods */
 	private static async Task Main(string[] args)
 	{
 		// Debug for now
-		var player = await JJx.Player.Load(args[0]);
-		//var world = await JJx.World.Load(args[0]);
 		Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
 		Raylib.InitWindow(0, 0, "Junk Jack X Editor");
 		Raylib.SetTargetFPS(144);
@@ -25,14 +30,42 @@ internal static class Program
 		InterfaceRenderer.InitTexture(_TexturePaths[0]);
 		ItemRenderer.InitTexture(_TexturePaths[1]);
 		// Main loop
-		var editor = new PlayerEditor();
 		while (!Raylib.WindowShouldClose())
 		{
+			// Update
+			switch (_CurrentScreen)
+			{
+				case Screen.Main:
+				{
+
+				} break;
+				case Screen.Player:
+				{
+					_PlayerEditor.Update(0.0f);
+				} break;
+				case Screen.World:
+				{
+					_WorldEditor.Update(0.0f);
+				} break;
+			}
+			// Drawing
 			Raylib.ClearBackground(Color.BLACK);
-			editor.Update(0.0f);
 			Raylib.BeginDrawing();
-				editor.Draw();
-				Raylib.DrawText($"Mouse Position: {Raylib.GetMousePosition()}", 0, 0, 12, Color.WHITE);
+				switch (_CurrentScreen)
+				{
+					case Screen.Main:
+					{
+
+					} break;
+					case Screen.Player:
+					{
+						_PlayerEditor.Draw();
+					} break;
+					case Screen.World:
+					{
+						_WorldEditor.Draw();
+					} break;
+				}
 			Raylib.EndDrawing();
 		}
 		InterfaceRenderer.UnloadTexture();
@@ -40,6 +73,9 @@ internal static class Program
 		Raylib.CloseWindow();
 	}
 	/* Class Properties */
+	private static Screen _CurrentScreen = Screen.Main;
+	private static PlayerEditor _PlayerEditor = null;
+	private static WorldEditor _WorldEditor = null;
 	private static string[] _TexturePaths = {
 		"data/gfx/interface.png",
 		"data/gfx/treasures.png",
