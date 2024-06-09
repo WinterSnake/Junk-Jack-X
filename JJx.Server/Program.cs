3ef5efb2c1ddbc5de5b21973359bd4e28356e1c0
Winter_Snake <blizzardsnake98@gmail.com>
2024-06-09 02:31:05 -0400

    add: server host

--- JJx.Server/Program.cs
+++ JJx.Server/Program.cs
@@ -4,7 +4,10 @@
 	Written By: Ryan Smith
 */
 using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using ENet.Managed;
 
 internal static class Program
 {
@@ -12,5 +15,30 @@ internal static class Program
 	private static async Task Main(string[] args)
 	{
 		Console.WriteLine("JJx: Server");
+		var address = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+		ManagedENet.Startup();
+		var server = new ENetHost(address, 16, 16);
+
+		while (true)
+		{
+			var @event = server.Service(TimeSpan.FromMilliseconds(0));
+			switch(@event.Type)
+			{
+				case ENetEventType.None: continue;
+				case ENetEventType.Connect:
+				{
+					Console.WriteLine($"User connected @{@event.Peer.GetRemoteEndPoint()}");
+				} break;
+				case ENetEventType.Disconnect:
+				{
+					Console.WriteLine($"User disconnected @{@event.Peer.GetRemoteEndPoint()}");
+				} break;
+				case ENetEventType.Receive:
+				{
+					Console.WriteLine("Received data from user..");
+					@event.Packet.Destroy();
+				} break;
+			}
+		}
 	}
 }
