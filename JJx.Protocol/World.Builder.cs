3733fc3c2c77d957fc6dc5819ac8cae0aa2bedfe
Winter_Snake <blizzardsnake98@gmail.com>
2026-08-01 01:10:43 -0400

    add: compressed segment builder

--- JJx.Protocol/World.Builder.cs
+++ JJx.Protocol/World.Builder.cs
@@ -6,6 +6,8 @@
 */
 
 using System;
+using System.Buffers;
+using System.Diagnostics;
 using JJx.Protocol.Packets;
 
 namespace JJx.Protocol;
@@ -14,31 +16,45 @@ public sealed class JJxWorldBuilder : IDisposable
 {
 	/* Constructor */
 	public JJxWorldBuilder(
-		(ushort, ushort) size, (ushort, ushort) spawn, (ushort, ushort) player
+		(ushort width, ushort) size, (ushort, ushort) spawn, (ushort, ushort) player,
+		uint worldSizeInBytes
 	)
 	{
 		this.Size = size;
 		this.Spawn = spawn;
 		this.Player = player;
+		this._Skyline = new ushort[size.width];
+		this._CompressedData = ArrayPool<byte>.Shared.Rent((int)worldSizeInBytes);
+		this._CompressedMemory = this._CompressedData.AsMemory(0, (int)worldSizeInBytes);
 	}
 	/* Instance Methods */
 	public void Build()
 	{
 
 	}
-	public void Dispose()
+	public void Dispose() => ArrayPool<byte>.Shared.Return(this._CompressedData);
+	public float PushToCompressedBuffer(ReadOnlySpan<byte> buffer)
 	{
-
+		Debug.Assert(this.CurrentSizeInBytes + buffer.Length <= this.TotalSizeInBytes);
+		buffer.CopyTo(this._CompressedMemory.Span.Slice(this.CurrentSizeInBytes));
+		this.CurrentSizeInBytes += buffer.Length;
+		return (float)this.CurrentSizeInBytes / (float)this.TotalSizeInBytes;
 	}
 	/* Static Methods */
 	public static JJxWorldBuilder FromWorldInfo(WorldInfoResponsePacket packet) => new(
 		packet.Size,
 		packet.Spawn,
-		packet.Player
+		packet.Player,
+		packet.WorldSizeInBytes
 	);
 	/* Properties */
 	public readonly (ushort Width, ushort Height) Size;
 	public readonly (ushort X, ushort Y) Spawn;
 	public readonly (ushort X, ushort Y) Player;
-	public ushort[] Skyline = Array.Empty<ushort>();
+	public Span<ushort> Skyline => this._Skyline;
+	private readonly ushort[] _Skyline;
+	private readonly byte[] _CompressedData;
+	private readonly Memory<byte> _CompressedMemory;
+	public int CurrentSizeInBytes { get; private set; } = 0;
+	public int TotalSizeInBytes => this._CompressedMemory.Length;
 }
